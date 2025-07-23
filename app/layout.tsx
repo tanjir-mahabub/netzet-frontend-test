fe859726d4deb5a8ef40f0299ff769ba62010e5d
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2025-07-23 18:02:43 +0600

    chore: add Figtree font using next/font and configure Tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Figtree } from 'next/font/google'
+import "@/styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const figtree = Figtree({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-figtree',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${figtree.variable}`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="font-figtree antialiased"
       >
         {children}
       </body>
