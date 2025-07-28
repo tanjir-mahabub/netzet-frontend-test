208d1565e4c801b324e092a4f9fdd4f5176e062d
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2025-07-28 23:32:10 +0600

    fix: resolve UI issue in Hero component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,11 +1,12 @@
 import HeroHeading from "./HeroHeading";
-import HeroFeatures from "./HeroFreatures";
+import HeroFeatures from "./HeroFeatures";
 import HeroCTA from "./HeroCTA";
 import Image from "next/image";
+import "./Hero.css";
 
 const Hero = () => {
     return (
-        <section className="relative flex flex-col-reverse md:flex-row justify-between items-center gap-6 px-2 sm:px-6 md:py-[70px] xl:py-[30px]">
+        <section className="relative flex flex-col-reverse md:flex-row justify-between items-center gap-6 px-2 sm:px-6 md:py-[70px] xl:py-[44px]">
             {/* Content */}
             <div className="flex-1 space-y-6 z-10 max-w-[516px] tracking-normal">
                 <HeroHeading />
@@ -14,8 +15,8 @@ const Hero = () => {
             </div>
 
             {/* Phone Background Image */}
-            <div className="relative flex-1 w-full mt-8 md:mt-0 md:absolute md:-top-12 md:right-0 md:h-full md:w-1/2 pointer-events-none">
-                <div className="relative w-full xl:w-[666px] min-h-[250px] md:min-h-[673px] mx-auto">
+            <div className="pointer-events-none relative flex xl:block h-full mt-8 w-full flex-1 md:absolute md:-top-12 md:right-[34px] md:mt-0 md:h-full md:w-1/2">
+                <div className="relative w-full xl:w-[666px] min-h-[426px] md:min-h-[673px] mx-auto">
                     <Image
                         src="/phone.png"
                         alt="App Preview"
